a6bed671e65c9a1b6d005552b0b66038b3d2343a
Hyun <hawksung1@gmail.com>
2020-06-28 15:12:41 +0900

    타일 장식물

---
+++ algorithm/4주차/가장먼노드.py
@@ -0,0 +1,7 @@
+def solution(begin, target, words):
+    answer = 0
+    return answer
+    
+
+if __name__ == "__main__":
+    solution()
---
+++ algorithm/4주차/타일장식물.py
@@ -0,0 +1,24 @@
+"""
+    피보나치 
+"""
+
+
+def solution(N):
+    made_list = make_list(N)
+    answer = cal_sum(made_list[len(made_list)-1], made_list[len(made_list)-2])
+    return answer
+
+def make_list(N):
+    result = [1]
+    num = 1
+    for i in range(N-1):
+        result.append(num)
+        num = num + result[len(result)-2]
+    return result
+
+def cal_sum(a, b):
+    return 2 * (a + (a + b))
+
+
+if __name__ == "__main__":
+    solution(6)
