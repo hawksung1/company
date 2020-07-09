4ed9fe5de82471dac634fde1b062034edd3cca5a
성현창 사원 빅데이터플랫폼2팀 (hyunchangsung@lgcns.com <82090@CORP.LGCNS.COM>
2020-07-09 17:11:59 +0900

    등굣길

---
+++ algorithm/4주차/등굣길.py
@@ -0,0 +1,17 @@
+def solution(m, n, puddles):
+    board = [[0]*(m+1) for i in range(n+1)]
+    board[1][1] = 1
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if i == 1 and j == 1:
+                continue
+            if [j, i] in puddles:
+                board[i][j] = 0
+            else:
+                board[i][j] = board[i-1][j] + board[i][j-1]
+    answer = board[n][m]
+    return answer % 1000000007
+
+if __name__ == "__main__":
+    tmp = solution(4,	3	,[[2, 2]]) # 4
+    print(tmp)
